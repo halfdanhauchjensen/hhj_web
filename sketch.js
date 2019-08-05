c57e2c2b1bc344a9f4c12080b930823d5f8f6f5b
Halfdan Hauch Jensen <halfdanhauchjensen@gmail.com>
2019-08-05 20:11:17 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -63,11 +63,12 @@ function particlesDraw(){
 
 function collage(){
 
-  var segmentWidth = ildDaabCover.width/3;
-  var segmentHeight = ildDaabCover.height/3;
+  //var totalWidth = 1000;
+  var segmentWidth = ildDaabCover.width/100;
+  var segmentHeight = ildDaabCover.height;
 
   for (var i = 0; i < 10; i++){
-    image(ildDaabCover.get(ildDaabCover.width/10*i,0,ildDaabCover.width/10, ildDaabCover.height), segmentWidth*i,0, segmentWidth, segmentHeight);
+    image(ildDaabCover.get(ildDaabCover.width/10*i,0,ildDaabCover.width/10, ildDaabCover.height), segmentWidth*i, 0, segmentWidth, 750);
   }
 
 
