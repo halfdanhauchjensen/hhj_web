38e551b03bda206df38307b64a49c7b899df9b93
Halfdan Hauch Jensen <halfdanhauchjensen@gmail.com>
2019-08-05 16:44:09 +0200

    Ild Daab cover added

--- sketch.js
+++ sketch.js
@@ -1,17 +1,19 @@
 
-var sketchHeight = 2000;
-var nrOfContentAreas = 4;
+var sketchHeight = 3000;
+var nrOfContentAreas = 5;
 var contentAreaHeight;
 let lightPathCover;
 let shadowPlayCover;
 let socialMegaphoneCover;
 let classicalConcertCover;
+let ildDaabCover;
 
 function preload() {
   lightPathCover = loadImage('pics/light_path/cover.jpg');
   shadowPlayCover = loadImage('pics/shadow_play/cover.jpg');
   socialMegaphoneCover = loadImage('pics/social_megaphone/cover.jpg');
   classicalConcertCover = loadImage('pics/classical_concert/cover.jpg');
+  ildDaabCover = loadImage('pics/ild_daab/cover.jpg');
 }
 
 function setup() {
@@ -24,11 +26,12 @@ function setup() {
 function draw() {
   // put drawing code here
   for (var i = 0; i < nrOfContentAreas; i++){
-  	rect(width/10,width/20+(i*contentAreaHeight), width-width/5, contentAreaHeight-width/10);
+  	//rect(width/10,width/20+(i*contentAreaHeight), width-width/5, contentAreaHeight-width/10);
     if (i==0) image(lightPathCover, width/10,width/20+(i*contentAreaHeight), width-width/5, contentAreaHeight-width/10);
     if (i==1) image(shadowPlayCover, width/10,width/20+(i*contentAreaHeight), width-width/5, contentAreaHeight-width/10);
     if (i==2) image(socialMegaphoneCover, width/10,width/20+(i*contentAreaHeight), width-width/5, contentAreaHeight-width/10);
     if (i==3) image(classicalConcertCover, width/10,width/20+(i*contentAreaHeight), width-width/5, contentAreaHeight-width/10);
+    if (i==4) image(ildDaabCover, width/10,width/20+(i*contentAreaHeight), width-width/5, contentAreaHeight-width/10);
   }
 }
 
