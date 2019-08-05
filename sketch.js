7a72c5875fd017a06d9b8674f622957a073bf14a
Halfdan Hauch Jensen <halfdanhauchjensen@gmail.com>
2019-08-05 15:10:54 +0200

    Readded files to the right branch

--- sketch.js
+++ sketch.js
@@ -1,7 +1,23 @@
+
+var sketchHeight = 2000;
+var nrOfContentAreas = 4;
+var contentAreaHeight;
+
 function setup() {
   // put setup code here
+  createCanvas(windowWidth, windowWidth/2*nrOfContentAreas);
+  contentAreaHeight = windowWidth/2;
 }
 
 function draw() {
   // put drawing code here
+  for (var i = 0; i < nrOfContentAreas; i++){
+  	fill(50+50*i);
+  	rect(0,i*contentAreaHeight, width, contentAreaHeight);
+  }
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowWidth/2*nrOfContentAreas);
+  contentAreaHeight = windowWidth/2;
 }
