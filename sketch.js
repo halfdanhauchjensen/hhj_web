8662e3334734d8a7e3e5544a66e8aed0334b1c21
Halfdan Hauch Jensen <halfdanhauchjensen@gmail.com>
2019-08-05 17:18:47 +0200

    experimenting with particle drawing

--- sketch.js
+++ sketch.js
@@ -8,6 +8,8 @@ let socialMegaphoneCover;
 let classicalConcertCover;
 let ildDaabCover;
 
+var particles = [];
+
 function preload() {
   lightPathCover = loadImage('pics/light_path/cover.jpg');
   shadowPlayCover = loadImage('pics/shadow_play/cover.jpg');
@@ -21,17 +23,25 @@ function setup() {
   createCanvas(windowWidth, windowWidth/2*nrOfContentAreas);
   contentAreaHeight = windowWidth/2;
   noFill();
+
+  for (var i = 0; i < 200; i++) {
+    particles[i] = new Particle(random(width), random(height));
+  }
 }
 
 function draw() {
+  particlesDraw();
+  
   // put drawing code here
   for (var i = 0; i < nrOfContentAreas; i++){
   	//rect(width/10,width/20+(i*contentAreaHeight), width-width/5, contentAreaHeight-width/10);
+    /*
     if (i==0) image(lightPathCover, width/10,width/20+(i*contentAreaHeight), width-width/5, contentAreaHeight-width/10);
     if (i==1) image(socialMegaphoneCover, width/10,width/20+(i*contentAreaHeight), width-width/5, contentAreaHeight-width/10);
     if (i==2) image(ildDaabCover, width/10,width/20+(i*contentAreaHeight), width-width/5, contentAreaHeight-width/10);
     if (i==3) image(classicalConcertCover, width/10,width/20+(i*contentAreaHeight), width-width/5, contentAreaHeight-width/10);
     if (i==4) image(shadowPlayCover, width/10,width/20+(i*contentAreaHeight), width-width/5, contentAreaHeight-width/10);
+    */
   }
 }
 
@@ -39,4 +49,12 @@ function windowResized() {
   resizeCanvas(windowWidth, windowWidth/2*nrOfContentAreas);
   contentAreaHeight = windowWidth/2;
   background(255);
+}
+
+function particlesDraw(){
+
+  for(var i = 0; i < particles.length; i++) {
+    particles[i].update();
+    particles[i].show(lightPathCover);
+  }
 }
